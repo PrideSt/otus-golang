245e448a947886e0070e00c473283312b6fe2620
Konstantin <stantonpride@gmail.com>
2020-09-10 22:30:50 +0300

    HW8 is completed (#8)
    * HW8 is completed

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,34 @@
 package main
 
-// RunCmd runs a command + arguments (cmd) with environment variables from env
-func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here
-	return
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+const (
+	ExitNormal = iota
+	ExitError
+)
+
+// RunCmd runs a command + arguments (cmd) with environment variables from env.
+func RunCmd(args []string, env Environment) (returnCode int) {
+	if len(args) < 1 {
+		fmt.Fprintln(os.Stderr, "not enough arguments, pass subcommand")
+		return ExitError
+	}
+
+	cmd := exec.Command(args[0], args[1:]...) //nolint:gosec
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Env = envToStrings(env)
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return ExitError
+	}
+
+	return ExitNormal
 }
