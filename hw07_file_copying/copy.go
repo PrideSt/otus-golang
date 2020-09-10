e576c3d63149eb1ebba9ad2d7b5a374b72c9398b
Konstantin <stantonpride@gmail.com>
2020-09-10 22:27:31 +0300

    HW7 is completed (#7)
    * HW7 is completed

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,14 +2,90 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
-	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrInvalidPath           = errors.New("invalid path")
+	ErrSetOffset             = errors.New("unable to set offset")
+	ErrFileStat              = errors.New("unable to get file's stat")
+	ErrCopy                  = errors.New("copy failed")
 )
 
-func Copy(fromPath string, toPath string, offset, limit int64) error {
-	// Place your code here
+// min return argument with minimum value.
+func min(a, b int64) int64 {
+	if a > b {
+		return b
+	}
+
+	return a
+}
+
+// closeOrErr shortcut for defer function, not forget close files.
+func closeOrErr(f io.Closer) {
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// Copy limit bytes (or while not EOF if limit = 0) from file specified by fromPath to
+// file specified by toPath. Coping starts at offset bytes from input.
+// Return error when offset large then input file or input file not seekable.
+func Copy(fromPath string, toPath string, offset int64, limit int64) error {
+	in, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("%w: open for reading failed %s", ErrInvalidPath, err)
+	}
+	defer in.Close()
+
+	inFileInfo, err := in.Stat()
+	if err != nil {
+		return fmt.Errorf("%w: %s", ErrFileStat, err)
+	}
+	inFileSize := inFileInfo.Size()
+
+	if offset > 0 {
+		if offset > inFileSize {
+			return fmt.Errorf(
+				"%w: given offset %d to large, file %q has len %d",
+				ErrOffsetExceedsFileSize,
+				offset,
+				fromPath,
+				inFileSize,
+			)
+		}
+		if _, err := in.Seek(offset, io.SeekStart); err != nil {
+			return fmt.Errorf("%w: set position %d in file %q failed %s", ErrSetOffset, offset, from, err)
+		}
+	}
+
+	var reader io.Reader = in
+	needToCopyBytes := inFileSize
+
+	if limit > 0 {
+		reader = io.LimitReader(in, limit)
+		needToCopyBytes = min(limit, inFileSize-offset)
+	}
+
+	writer, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("%w: unable to open file %q for writing %s", ErrInvalidPath, toPath, err)
+	}
+	defer closeOrErr(writer)
+
+	bar := pb.Full.Start64(needToCopyBytes)
+	defer bar.Finish()
+
+	barReader := bar.NewProxyReader(reader)
+	if _, err = io.Copy(writer, barReader); err != nil {
+		return fmt.Errorf("%w: %s", ErrCopy, err)
+	}
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,237 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// fileGetData returns content of file specifies by filePath
+func fileGetData(filePath string) []byte {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer closeOrErr(file)
+
+	expData, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return expData
+}
+
+// filesEqual compare content of two files
+func filesEqual(t *testing.T, expectedPath, actualPath string) {
+	expDt := fileGetData(expectedPath)
+	actDt := fileGetData(actualPath)
+
+	require.Equal(t, expDt, actDt)
+}
+
+// fileSizeEqual compare whether file specified by path has size equal expectedSize
+func fileSizeEqual(t *testing.T, path string, expectedSize int64) {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer closeOrErr(f)
+
+	inFileInfo, err := f.Stat()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	require.Equal(t, expectedSize, inFileInfo.Size())
+}
 
 func TestCopy(t *testing.T) {
-	// Place your code here
+	for _, tt := range [...]struct {
+		name         string
+		inputPath    string
+		offset       int64
+		limit        int64
+		expectedPath string
+		err          error
+		errMsg       string
+	}{
+		{
+			name:         `copy full file`,
+			inputPath:    `testdata/input.txt`,
+			expectedPath: `testdata/out_offset0_limit0.txt`,
+		},
+		{
+			name:         `copy with limit`,
+			inputPath:    `testdata/input.txt`,
+			limit:        10,
+			expectedPath: `testdata/out_offset0_limit10.txt`,
+		},
+		{
+			name:         `copy with large limit`,
+			inputPath:    `testdata/input.txt`,
+			limit:        1000,
+			expectedPath: `testdata/out_offset0_limit1000.txt`,
+		},
+		{
+			name:         `copy with limit out of the input file`,
+			inputPath:    `testdata/input.txt`,
+			limit:        10000,
+			expectedPath: `testdata/out_offset0_limit10000.txt`,
+		},
+		{
+			name:         `copy with limit and offset`,
+			inputPath:    `testdata/input.txt`,
+			offset:       100,
+			limit:        1000,
+			expectedPath: `testdata/out_offset100_limit1000.txt`,
+		},
+		{
+			name:         `copy with limit and large offset, offset with limit is out of input`,
+			inputPath:    `testdata/input.txt`,
+			offset:       6000,
+			limit:        1000,
+			expectedPath: `testdata/out_offset6000_limit1000.txt`,
+		},
+		{
+			name:      `offset is out of input`,
+			inputPath: `testdata/input.txt`,
+			offset:    99999,
+			err:       ErrOffsetExceedsFileSize,
+			errMsg:    `offset exceeds file size: given offset 99999 to large, file "testdata/input.txt" has len 6617`,
+		},
+		{
+			name:      `input file not exists`,
+			inputPath: `testdata/no-input-file.txt`,
+			err:       ErrInvalidPath,
+			errMsg:    `invalid path: open for reading failed open testdata/no-input-file.txt: no such file or directory`,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			var tmpFile *os.File
+			var err error
+
+			if len(tt.expectedPath) > 0 {
+				baseDir := "/tmp"
+				tmpFile, err = ioutil.TempFile(baseDir, "test-copy")
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				closeOrErr(tmpFile)
+
+				defer func() {
+					err := os.Remove(tmpFile.Name())
+					if err != nil {
+						log.Fatal(err)
+					}
+				}()
+			}
+
+			if tmpFile == nil {
+				err = Copy(tt.inputPath, "", tt.offset, tt.limit)
+			} else {
+				err = Copy(tt.inputPath, tmpFile.Name(), tt.offset, tt.limit)
+			}
+
+			if tt.err == nil {
+				require.NoError(t, err)
+				filesEqual(t, tt.expectedPath, tmpFile.Name())
+			} else {
+				require.EqualError(t, err, tt.errMsg)
+				require.True(t, errors.Is(err, tt.err))
+			}
+		})
+	}
+}
+
+func TestDevRandom(t *testing.T) {
+	for _, tt := range [...]struct {
+		name   string
+		offset int64
+		limit  int64
+		err    error
+		errMsg string
+	}{
+		{
+			name:  `copy with limit`,
+			limit: 1000,
+		},
+		{
+			name:   `copy with limit and offset`,
+			offset: 100,
+			limit:  1000,
+			err:    ErrOffsetExceedsFileSize,
+			errMsg: `offset exceeds file size: given offset 100 to large, file "/dev/urandom" has len 0`,
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			var tmpFile *os.File
+			var err error
+
+			inputPath := "/dev/urandom"
+			baseDir := "/tmp"
+			tmpFile, err = ioutil.TempFile(baseDir, "test-dev-random")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			closeOrErr(tmpFile)
+
+			defer func() {
+				err := os.Remove(tmpFile.Name())
+				if err != nil {
+					log.Fatal(err)
+				}
+			}()
+
+			err = Copy(inputPath, tmpFile.Name(), tt.offset, tt.limit)
+
+			if tt.err == nil {
+				require.NoError(t, err)
+				fileSizeEqual(t, tmpFile.Name(), tt.limit)
+			} else {
+				require.EqualError(t, err, tt.errMsg)
+				require.True(t, errors.Is(err, tt.err))
+			}
+		})
+	}
+}
+
+func TestEmptyToPath(t *testing.T) {
+	err := Copy(`testdata/input.txt`, "", 0, 0)
+
+	require.EqualError(t, err, `invalid path: unable to open file "" for writing open : no such file or directory`)
+	require.True(t, errors.Is(err, ErrInvalidPath))
+}
+
+func TestInputIsADir(t *testing.T) {
+	var tmpFile *os.File
+	var err error
+
+	inputPath := "testdata/its_a_dir"
+
+	baseDir := "/tmp"
+	tmpFile, err = ioutil.TempFile(baseDir, "test-from-dir")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	closeOrErr(tmpFile)
+
+	defer func() {
+		err := os.Remove(tmpFile.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	err = Copy(inputPath, tmpFile.Name(), 0, 0)
+
+	require.EqualError(t, err, "copy failed: read testdata/its_a_dir: is a directory")
+	require.True(t, errors.Is(err, ErrCopy))
 }
