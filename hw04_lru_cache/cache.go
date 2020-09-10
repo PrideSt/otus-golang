fcd8156a198c56c93d2a6c12d9d1924d10cc3d6d
Konstantin <stantonpride@gmail.com>
2020-09-10 22:17:58 +0300

    HW4 is completed (#4)
    * HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,22 +1,81 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
+import (
+	"sync"
+)
+
 type Key string
 
 type Cache interface {
-	// Place your code here
-}
-
-type lruCache struct {
-	// Place your code here:
-	// - capacity
-	// - queue
-	// - items
+	Set(key Key, value interface{}) bool // Добавить значение в кэш по ключу
+	Get(key Key) (interface{}, bool)     // Получить значение из кэша по ключу
+	Clear()                              // Очистить кэш
 }
 
 type cacheItem struct {
-	// Place your code here
+	key   Key
+	value interface{}
 }
 
+type lruCache struct {
+	capacity int
+	queue    List
+	items    map[Key]*Item
+	mu       sync.Mutex
+}
+
+// NewCache creates new Cache instance.
 func NewCache(capacity int) Cache {
-	return &lruCache{}
+	return &lruCache{
+		capacity: capacity,
+		queue:    NewList(),
+		items:    make(map[Key]*Item, capacity),
+	}
+}
+
+// Get returns {value, true} whether element with key stored in cache and {nil, false} pair otherwise.
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if item, ok := c.items[key]; ok {
+		c.queue.MoveToFront(item)
+
+		return item.Value.(*cacheItem).value, true
+	}
+
+	return nil, false
+}
+
+// Set stores value in cache with key, returns whether element was already in cache.
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	if item, ok := c.items[key]; ok {
+		c.queue.MoveToFront(item)
+		item.Value.(*cacheItem).value = value
+
+		return true
+	}
+
+	if c.capacity == c.queue.Len() {
+		lastItem := c.queue.Back()
+		c.queue.Remove(lastItem)
+		delete(c.items, lastItem.Value.(*cacheItem).key)
+	}
+
+	c.queue.PushFront(&cacheItem{key, value})
+	c.items[key] = c.queue.Front()
+
+	return false
+}
+
+// Clear removes all elements from cache.
+func (c *lruCache) Clear() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c.queue = NewList()
+	c.items = make(map[Key]*Item, c.capacity)
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,17 +1,136 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
 type List interface {
-	// Place your code here
+	Len() int                      // длина списка
+	Front() *Item                  // первый Item
+	Back() *Item                   // последний Item
+	PushFront(v interface{}) *Item // добавить значение в начало
+	PushBack(v interface{}) *Item  // добавить значение в конец
+	Remove(i *Item)                // удалить элемент
+	MoveToFront(i *Item)           // переместить элемент в начало
 }
 
-type listItem struct {
-	// Place your code here
+type Item struct {
+	Next  *Item
+	Prev  *Item
+	Value interface{}
 }
 
 type list struct {
-	// Place your code here
+	first *Item
+	last  *Item
+	len   int
 }
 
+// NewList creates new instance of list.
 func NewList() List {
 	return &list{}
+}
+
+// Len returns count of elements in list.
+func (l list) Len() int {
+	return l.len
+}
+
+// Front returns the first element from list.
+func (l list) Front() *Item {
+	return l.first
+}
+
+// Back returns the last element from list.
+func (l list) Back() *Item {
+	return l.last
+}
+
+// PushFront add new element at the begin of list.
+func (l *list) PushFront(v interface{}) *Item {
+	i := &Item{
+		Next:  l.first,
+		Value: v,
+	}
+
+	if l.first == nil {
+		l.last = i
+	} else {
+		l.first.Prev = i
+	}
+
+	l.first = i
+	l.len++
+
+	return i
+}
+
+// PushBack add new element at the end of list.
+func (l *list) PushBack(v interface{}) *Item {
+	i := &Item{
+		Prev:  l.last,
+		Value: v,
+	}
+
+	if l.last == nil {
+		l.first = i
+	} else {
+		l.last.Next = i
+	}
+
+	l.last = i
+	l.len++
+
+	return i
+}
+
+// Remove delete element i from list.
+func (l *list) Remove(i *Item) {
+	if l.len < 2 {
+		l.first = nil
+		l.last = nil
+		l.len = 0
+
+		return
+	}
+
+	if i.Prev != nil {
+		i.Prev.Next = i.Next
+	} else {
+		l.first = i.Next
+		i.Next.Prev = nil
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.last = i.Prev
+		i.Prev.Next = nil
+	}
+
+	l.len--
+}
+
+// MoveToFront move element i from current position of list to begin.
+func (l *list) MoveToFront(i *Item) {
+	if l.len < 2 {
+		return
+	}
+
+	// i is the first element, nothing to do
+	if i.Prev == nil {
+		return
+	}
+
+	// remove item, bind left and right elements together
+	i.Prev.Next = i.Next
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		// remove last element
+		l.last = i.Prev
+	}
+
+	// place element i at begin
+	// we can use PushFront, but don't want to copy value and create new Item node
+	i.Prev = nil
+	i.Next = l.first
+	l.first.Prev = i
+	l.first = i
 }
