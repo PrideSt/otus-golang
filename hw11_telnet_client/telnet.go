a8baee160e599041aeb5352cb23ca1ff02d3c15a
Konstantin <stantonpride@gmail.com>
2020-09-18 21:06:03 +0300

    HW11 is completed (#11)
    * HW11 is completed

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,18 +1,70 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"log"
+	"net"
 	"time"
 )
 
 type TelnetClient interface {
-	// Place your code here
+	Connect() error
+	Close() error
+	Send() error
+	Receive() error
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here
+	tcpAddr, err := net.ResolveTCPAddr("tcp4", address)
+	if err != nil {
+		log.Fatalf("invalid address %q, %s", address, err)
+	}
+
+	return &TCPTelnetClient{
+		address: tcpAddr,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+type TCPTelnetClient struct {
+	address net.Addr
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	con     net.Conn
+}
+
+func (c *TCPTelnetClient) Connect() error {
+	con, err := net.DialTimeout("tcp", c.address.String(), c.timeout)
+	if err != nil {
+		return err
+	}
+	c.con = con
+
 	return nil
 }
 
-// Place your code here
-// P.S. Author's solution takes no more than 50 lines
+func (c *TCPTelnetClient) Close() error {
+	if err := c.in.Close(); err != nil {
+		return fmt.Errorf("close connection fault: %w", err)
+	}
+
+	if err := c.con.Close(); err != nil {
+		return fmt.Errorf("close connection fault: %w", err)
+	}
+
+	return nil
+}
+
+func (c *TCPTelnetClient) Send() error {
+	_, err := io.Copy(c.con, c.in)
+	return err
+}
+
+func (c *TCPTelnetClient) Receive() error {
+	_, err := io.Copy(c.out, c.con)
+	return err
+}
--- hw11_telnet_client/telnet_test.go
+++ hw11_telnet_client/telnet_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"io/ioutil"
+	"log"
 	"net"
 	"sync"
 	"testing"
@@ -12,6 +13,8 @@ import (
 )
 
 func TestTelnetClient(t *testing.T) {
+	log.SetOutput(ioutil.Discard)
+
 	t.Run("basic", func(t *testing.T) {
 		l, err := net.Listen("tcp", "127.0.0.1:")
 		require.NoError(t, err)
@@ -61,5 +64,90 @@ func TestTelnetClient(t *testing.T) {
 		}()
 
 		wg.Wait()
+	})
+
+	t.Run("close connection", func(t *testing.T) {
+		l, err := net.Listen("tcp", "127.0.0.1:")
+		require.NoError(t, err)
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			in := &bytes.Buffer{}
+			out := &bytes.Buffer{}
+
+			timeout, err := time.ParseDuration("10s")
+			require.NoError(t, err)
+
+			client := NewTelnetClient(l.Addr().String(), timeout, ioutil.NopCloser(in), out)
+			require.NoError(t, client.Connect())
+			defer func() { require.NoError(t, client.Close()) }()
+
+			err = client.Receive()
+			require.Error(t, err)
+			require.EqualError(t, err.(*net.OpError).Err, "read: connection reset by peer")
+		}()
+
+		time.Sleep(100 * time.Millisecond)
+		_ = l.Close()
+
+		wg.Wait()
+	})
+
+	t.Run("input EOF", func(t *testing.T) {
+		l, err := net.Listen("tcp", "127.0.0.1:")
+		require.NoError(t, err)
+		defer func() { require.NoError(t, l.Close()) }()
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+
+			in := &bytes.Buffer{}
+			out := &bytes.Buffer{}
+
+			timeout, err := time.ParseDuration("10s")
+			require.NoError(t, err)
+
+			client := NewTelnetClient(l.Addr().String(), timeout, ioutil.NopCloser(in), out)
+			require.NoError(t, client.Connect())
+			defer func() { require.NoError(t, client.Close()) }()
+
+			err = client.Send() // send empty buffer
+			require.NoError(t, err)
+		}()
+
+		go func() {
+			defer wg.Done()
+
+			conn, err := l.Accept()
+			require.NoError(t, err)
+			require.NotNil(t, conn)
+			defer func() { require.NoError(t, conn.Close()) }()
+
+			request := make([]byte, 1024)
+			_, err = conn.Read(request)
+			require.EqualError(t, err, "EOF")
+		}()
+
+		wg.Wait()
+	})
+
+	t.Run("no server by address", func(t *testing.T) {
+		in := &bytes.Buffer{}
+		out := &bytes.Buffer{}
+
+		timeout, err := time.ParseDuration("10s")
+		require.NoError(t, err)
+
+		client := NewTelnetClient("127.0.0.1:9876", timeout, ioutil.NopCloser(in), out)
+		err = client.Connect()
+		require.Error(t, err)
+		require.EqualError(t, err.(*net.OpError).Err, "connect: connection refused")
 	})
 }
